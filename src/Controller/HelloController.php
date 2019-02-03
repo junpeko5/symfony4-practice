812f370e7a825d1a6675c9493d9eb46e07ae6202
junpeko5 <junpeko5@gmail.com>
2019-02-03 22:30:05 +0900

    RedirectResponseクラスを使い、リダイレクトを行う

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,17 +11,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloController extends AbstractController
 {
   /**
-   * @Route("/hello/{name}/{pass}", name="hello")
+   * @Route("/hello", name="hello")
    */
     public function index(Request $request)
     {
-        $name = $request->get('name');
-        $pass = $request->get('pass');
         $result = '<html><body>';
         $result .= '<h1>Parameter</h1>';
-        $result .= '<p>name: ' . $name . '</p>';
-        $result .= '<p>pass: ' . $pass . '</p>';
+        $result .= '<p>This is index page.</p>';
         $result .= '</body></html>';
         return new Response($result);
+    }
+
+    /**
+     * @Route("/other/{domain}", name="other")
+     */
+    public function other(Request $request, $domain='')
+    {
+        if ($domain == '') {
+            return $this->redirect('/hello');
+        } else {
+            return new RedirectResponse("http://{$domain}.com");
+        }
     }
 }
