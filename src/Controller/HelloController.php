a5bd043f5877c987b7e91ca4cefde990f806f97d
junpeko5 <junpeko5@gmail.com>
2019-02-03 22:59:44 +0900

    特定のエラーコードを表示する

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -19,19 +19,72 @@ class HelloController extends AbstractController
    */
     public function index(Request $request)
     {
-        $encoders = [new XmlEncoder()];
-        $normalizers = [new ObjectNormalizer()];
-        $serializer = new Serializer($normalizers, $encoders);
+        $content = <<< EOM
+<html>
+<head>
+<title>Hello</title>
+</head>
+<body>
+<p>This is Symfony sample page.</p>
+</body>
+</html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_OK,
+            ['content-type' => 'text/html']
+        );
+        return $response;
+    }
 
-        $data = [
-            'name' => ['first' => 'Hanako', 'second' => 'Tanaka'],
-            'age' => 29,
-            'email' => 'hanako@flower.san',
-        ];
-        $response = new Response();
-        $response->headers->set('Content-Type', 'xml');
-        $result = $serializer->serialize($data, 'xml');
-        $response->setContent($result);
+
+    /**
+     * @Route("/notfound", name="notfound")
+     * @param Request $request
+     * @return Response
+     */
+    public function notfound(Request $request)
+    {
+        $content = <<< EOM
+<html>
+<head>
+<title>NotFound 404</title>
+</head>
+<body>
+<p>Not found</p>
+</body>
+</html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_NOT_FOUND,
+            ['Content-Type' => 'text/html']
+        );
+        return $response;
+    }
+
+    /**
+     * @Route("/error", name="error")
+     * @param Request $request
+     * @return Response
+     */
+    public function error(Request $request)
+    {
+        $content = <<< EOM
+<html>
+<head>
+<title>Error</title>
+</head>
+<body>
+<h1>Error! 500</h1>
+</body>
+</html>
+EOM;
+        $response = new Response(
+            $content,
+            Response::HTTP_INTERNAL_SERVER_ERROR,
+            ['content-type' => 'text/html']
+        );
         return $response;
     }
 }
