b35a47a03a6c3904ec9d79e77d40dde37f6ca54f
junpeko5 <junpeko5@gmail.com>
2019-02-04 21:15:12 +0900

    フォームをオブジェクトで管理する

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,8 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
 class HelloController extends AbstractController
@@ -17,21 +19,70 @@ class HelloController extends AbstractController
      */
     public function index(Request $request)
     {
-        $form = $this->createFormBuilder()
-            ->add('input', TextType::class)
+        $person = new Person();
+        $person->setName('taro')
+            ->setAge(36)
+            ->setMail('taro@yamada.kun');
+
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('mail', EmailType::class)
             ->add('save', SubmitType::class, ['label' => 'Click'])
             ->getForm();
+
         if ($request->getMethod() === 'POST') {
             $form->handleRequest($request);
-            $msg = 'こんにちは、' . $form->get('input')->getData() . 'さん!';
+            $obj = $form->getData();
+            $msg = 'Name: ' . $obj->getName() . '<br>' . 'Age: ' . $obj->getAge() . '<br>' . 'Mail: '. $obj->getMail();
         } else {
-            $msg = 'お名前をどうぞ';
+            $msg = 'お名前をどうぞ！';
         }
 
         return $this->render('hello/index.html.twig', [
-            'title' => 'Hello',
-            'message' => $msg,
-            'form' => $form->createView(),
+           'title' => 'Hello',
+           'message' => $msg,
+           'form' => $form->createView()
         ]);
+    }
+}
+
+class Person
+{
+    protected $name;
+    protected $age;
+    protected $mail;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+        return $this;
+    }
+
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+        return $this;
     }
 }
