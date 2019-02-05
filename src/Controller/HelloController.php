173b634b69375c82441550a82deb1bdd97c3f834
junpeko5 <junpeko5@gmail.com>
2019-02-05 22:50:24 +0900

    IDでエンティティを検索する

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Person;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -51,5 +53,49 @@ class HelloController extends AbstractController
            'title' => 'Hello',
            'data' => $data,
         ]);
+    }
+
+    /**
+     * @Route("/find", name="find")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function find(Request $request)
+    {
+        $formObj = new FindForm();
+        $form = $this->createFormBuilder($formObj)
+            ->add('find', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();
+        if ($request->getMethod() === 'POST') {
+            $form->handleRequest($request);
+            $findStr = $form->getData()->getFind();
+            $repository = $this->getDoctrine()
+                ->getRepository(Person::class);
+            $result = $repository->find($findStr);
+        } else {
+            $result = null;
+        }
+        return $this->render('hello/find.html.twig', [
+           'title' => 'Hello',
+           'form' => $form->createView(),
+           'data' => $result,
+        ]);
+    }
+}
+
+class FindForm
+{
+    private $find;
+
+    public function getFind()
+    {
+        var_dump($this->find);
+        return $this->find;
+    }
+
+    public function setFind($find)
+    {
+        $this->find = $find;
     }
 }
