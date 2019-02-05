74b571d9d2985da927885c8d1a32fe2608e8a304
junpeko5 <junpeko5@gmail.com>
2019-02-05 23:26:58 +0900

    レコードの追加、更新

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Person;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Bundle\FrameworkBundle;
 
 class HelloController extends AbstractController
 {
@@ -55,9 +56,11 @@ class HelloController extends AbstractController
         ]);
     }
 
+
     /**
      * @Route("/find/{id}", name="find")
      * @param Request $request
+     * @param Person $person
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function find(Request $request, Person $person)
@@ -66,6 +69,65 @@ class HelloController extends AbstractController
            'title' => 'Hello',
            'data' => $person,
         ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function create(Request $request)
+    {
+        $person = new Person();
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        if ($request->getMethod() === 'POST') {
+            $form->handleRequest($request);
+            $person = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($person);
+            $manager->flush();
+            return $this->redirect('/hello');
+        } else {
+            return $this->render('hello/create.html.twig', [
+                'title' => 'Hello',
+                'message' => 'Create Entity',
+                'form' => $form->createView(),
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/update/{id}", name="update")
+     */
+    public function update(Request $request, Person $person)
+    {
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('name', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();
+
+        if ($request->getMethod() === 'POST') {
+            $form->handleRequest($request);
+            $person = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->flush();
+            return $this->redirect('/hello');
+        } else {
+            return $this->render('/hello/create.html.twig', [
+               'title' => 'Hello',
+               'message' => 'Update Entity id=' . $person->getId(),
+               'form' => $form->createView(),
+            ]);
+        }
     }
 }
 
