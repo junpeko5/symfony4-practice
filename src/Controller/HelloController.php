c9345ec07d47d15579af7868c40283a39bd6e6fe
junpeko5 <junpeko5@gmail.com>
2019-02-07 00:45:52 +0900

    pathによるフォルダ設定

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -53,7 +53,7 @@ class HelloController extends AbstractController
     public function hello(Request $request)
     {
         $finder = new Finder();
-        $finder->files()->depth('<=1')->contains('* @')->in('../src/');
+        $finder->files()->depth('<=1')->path('src')->path('templates')->in('../');
         return $this->render('hello/hello.html.twig', [
            'title' => 'Hello',
            'message' => 'get file/folder',
