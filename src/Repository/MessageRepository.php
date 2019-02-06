d59f2bba77b2014363d17e5e16eb7e40134be3fc
junpeko5 <junpeko5@gmail.com>
2019-02-06 21:53:10 +0900

    MessageRepositoryの修正

--- src/Repository/MessageRepository.php
+++ src/Repository/MessageRepository.php
@@ -9,7 +9,6 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 /**
  * @method Message|null find($id, $lockMode = null, $lockVersion = null)
  * @method Message|null findOneBy(array $criteria, array $orderBy = null)
- * @method Message[]    findAll()
  * @method Message[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class MessageRepository extends ServiceEntityRepository
@@ -17,6 +16,11 @@ class MessageRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Message::class);
+    }
+
+    public function findAll()
+    {
+        return $this->findBy([], ['posted' => 'DESC']);
     }
 
     // /**
