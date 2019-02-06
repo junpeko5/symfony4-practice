d7888f539834f86180fac604708649f2ace4000e
junpeko5 <junpeko5@gmail.com>
2019-02-07 00:27:43 +0900

    ページネーションの作成

--- src/Repository/MessageRepository.php
+++ src/Repository/MessageRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Message;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
@@ -21,6 +22,25 @@ class MessageRepository extends ServiceEntityRepository
     public function findAll()
     {
         return $this->findBy([], ['posted' => 'DESC']);
+    }
+
+    public function getPage($currentPage = 1, $limit = 5)
+    {
+        $query = $this->createQueryBuilder('p')
+            ->orderBy('p.posted', 'DESC')
+            ->getQuery();
+        $paginator = $this->paginate($query, $currentPage, $limit);
+        return $paginator;
+    }
+
+    public function paginate($dql, $page = 1, $limit = 5)
+    {
+        $paginator = new Paginator($dql);
+        $paginator
+            ->getQuery()
+            ->setFirstResult($limit * ($page - 1))
+            ->setMaxResults($limit);
+        return $paginator;
     }
 
     // /**
